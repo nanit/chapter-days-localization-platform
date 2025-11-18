a2ca7aa636c91c43fa8f5ed09a1b9badc4d32f95
Kvouthe <rofapb724@gmail.com>
2025-11-18 18:41:46 +0200

    Update

--- server/data/src/jvmMain/kotlin/com/nanit/localization/server/data/database/DatabaseDriverFactory.jvm.kt
+++ server/data/src/jvmMain/kotlin/com/nanit/localization/server/data/database/DatabaseDriverFactory.jvm.kt
@@ -3,16 +3,8 @@ package com.nanit.localization.server.data.database
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 
-//actual class DatabaseDriverFactory {
-//    actual fun createDriver(): SqlDriver {
-//        val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-//        LocalizationDatabase.Schema.create(driver)
-//        return driver
-//    }
-//}
-
-class JvmSqlDriverProvider : SqlDriverProvider {
-    override fun createDriver(): SqlDriver {
+actual class DatabaseDriverFactory {
+    actual fun createDriver(): SqlDriver {
         val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
         LocalizationDatabase.Schema.create(driver)
         return driver
