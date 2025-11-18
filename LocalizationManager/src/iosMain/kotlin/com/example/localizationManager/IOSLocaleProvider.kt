6ec42f57a843565e13d3d8d41a63327a2e92726f
yacovrosenberg <yacov.rosenberg@nanit.com>
2025-11-18 16:59:51 +0200

    Move the database code to sdk module in order to solve the issues

--- LocalizationManager/src/iosMain/kotlin/com/example/localizationManager/IOSLocaleProvider.kt
+++ LocalizationManager/src/iosMain/kotlin/com/example/localizationManager/IOSLocaleProvider.kt
@@ -3,12 +3,16 @@ package com.example.localizationManager
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.example.localizationManager.database.database.SqlDriverProvider
-import com.nanit.localization.database.IosSqlDriverProvider
 import com.nanit.localization.database.LocalizationDatabase
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import platform.Foundation.*
+import platform.Foundation.NSCurrentLocaleDidChangeNotification
+import platform.Foundation.NSLocale
+import platform.Foundation.NSNotificationCenter
+import platform.Foundation.NSOperationQueue
+import platform.Foundation.currentLocale
+import platform.Foundation.languageCode
 
 class IOSLocaleProvider: LocaleProvider {
     override fun getCurrentLocale(): LocaleInfo =
